f43c3abab9e7058603b9ece80c0be6ca96ec7648
Alibek Zhubekov <alibek1991@gmail.com>
2023-07-13 13:34:07 +0600

    Create Database Model

--- crm_app/website/models.py
+++ crm_app/website/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
-# Create your models here.
 
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+    phone = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=64)
+    zipcode = models.CharField(max_length=64)
 
-class Record:
-    pass
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
